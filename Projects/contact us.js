6fb9198da46a1dffc345047b3560ecd14dbbf341
SMShafeen <123641643+SMShafeen@users.noreply.github.com>
2023-03-16 14:38:14 +0530

    Add files via upload

---
+++ Projects/contact us.js
@@ -0,0 +1,28 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+        parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus")
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+}
+);
+
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
